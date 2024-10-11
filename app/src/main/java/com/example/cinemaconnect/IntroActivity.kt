9b881ab1b666843619444cc57628380301e7cd8c
Amlan101 <amlan.sahooricky@gmail.com>
2024-10-11 11:52:26 +0530

    Added the Intro page as well as the locally needed drawables

---
+++ app/src/main/java/com/example/cinemaconnect/IntroActivity.kt
@@ -0,0 +1,28 @@
+package com.example.cinemaconnect
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.example.cinemaconnect.databinding.ActivityIntroBinding
+
+class IntroActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityIntroBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startButton.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+    }
+}
