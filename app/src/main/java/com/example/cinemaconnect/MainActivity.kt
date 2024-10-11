dbd54267d9e4bc1402d50d98e00b6a199789309e
Amlan101 <amlan.sahooricky@gmail.com>
2024-10-11 13:21:57 +0530

    Completed the UI of the Dashboard page

--- app/src/main/java/com/example/cinemaconnect/MainActivity.kt
+++ app/src/main/java/com/example/cinemaconnect/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.cinemaconnect
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.cinemaconnect.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 }
