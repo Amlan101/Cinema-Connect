c47c3f2ddd93e210880a3970340c70be8a9e595f
Amlan101 <amlan.sahooricky@gmail.com>
2024-10-12 14:23:55 +0530

    Completed the Slider of the Dashboard Page

--- app/src/main/java/com/example/cinemaconnect/MainActivity.kt
+++ app/src/main/java/com/example/cinemaconnect/MainActivity.kt
@@ -1,17 +1,106 @@
 package com.example.cinemaconnect
 
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
+import androidx.viewpager2.widget.ViewPager2
+import com.example.cinemaconnect.adapters.SliderAdapter
 import com.example.cinemaconnect.databinding.ActivityMainBinding
+import com.example.cinemaconnect.models.SliderItems
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import kotlin.math.abs
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var database: FirebaseDatabase
+
+    private val sliderHandler = Handler()
+    private val sliderRunnable = Runnable {
+        binding.sliderViewPager2.currentItem += 1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        database = FirebaseDatabase.getInstance()
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+
+        initBanner()
+
     }
+
+    private fun initBanner() {
+        val myRef: DatabaseReference = database.getReference("Banners")
+        binding.progressBarSlider.visibility = View.VISIBLE
+
+        myRef.addListenerForSingleValueEvent(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val lists = mutableListOf<SliderItems>()
+                for(childSnapshot in snapshot.children){
+                    val list = childSnapshot.getValue(SliderItems::class.java)
+                    if(list != null) lists.add(list)
+                }
+                binding.progressBarSlider.visibility = View.GONE 
+                banners(lists)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.e("Firebase Error", error.message)
+            }
+
+        })
+    }
+
+    private fun banners(lists: MutableList<SliderItems>) {
+        binding.sliderViewPager2.adapter = SliderAdapter(lists, binding.sliderViewPager2)
+        binding.sliderViewPager2.clipToPadding = false
+        binding.sliderViewPager2.clipChildren = false
+        binding.sliderViewPager2.offscreenPageLimit = 3
+        binding.sliderViewPager2.getChildAt(0).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
+
+        val compositePageTransformer = CompositePageTransformer().apply {
+            addTransformer(MarginPageTransformer(40))
+            addTransformer { page, position ->
+                val r = 1 - abs(position)
+                page.scaleY = 0.85f + r * 0.15f
+            }
+        }
+        binding.sliderViewPager2.setPageTransformer(compositePageTransformer)
+        binding.sliderViewPager2.currentItem = 1
+        binding.sliderViewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                sliderHandler.removeCallbacks(sliderRunnable)
+                sliderHandler.postDelayed(sliderRunnable, 2000)
+            }
+        })
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sliderHandler.removeCallbacks(sliderRunnable)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sliderHandler.postDelayed(sliderRunnable, 2000)
+    }
+
 }
---
+++ app/src/main/java/com/example/cinemaconnect/models/SliderItems.kt
@@ -0,0 +1,5 @@
+package com.example.cinemaconnect.models
+
+data class SliderItems (
+    val image: String = ""
+)
